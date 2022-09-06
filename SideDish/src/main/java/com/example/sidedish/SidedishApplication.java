87e227e2f52772352ea8643f490b7ad168b88ce8
Bradley Steben <snadley10@gmail.com>
2022-09-06 16:42:27 -0400

    First5

--- SideDish/src/main/java/com/example/sidedish/SidedishApplication.java
+++ SideDish/src/main/java/com/example/sidedish/SidedishApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SidedishApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SidedishApplication.class, args);
-    }
+	public static void main(String[] args) {
+
+		Application context =SpringApplication.run(SidedishApplication.class, args);
+	}
 
 }
